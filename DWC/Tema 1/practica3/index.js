e40077d0d2c7a96464e7cd8f2b6cfc08f6085a0c
JordiVU <jordivazquezuribe@gmail.com>
2025-10-23 18:16:31 +0200

    Actualizacion Practica3  - Completada

--- DWC/Tema 1/practica3/index.js
+++ DWC/Tema 1/practica3/index.js
@@ -1,15 +1,22 @@
-// Completa este archivo
+// Jordi Vázquez Uribe
+// JavaScript del programa principal para gestionar el AÑADIR/MOVER/BORRAR una tarjeta.
 document.addEventListener('DOMContentLoaded', () => {
     const form = document.getElementById('task-form');
     const imgPreview = document.getElementById('image-preview');
     const template = document.getElementById('task-template');
     const divPrincipal = document.getElementById('divPrincipal');
+    const today = new Date().toISOString().split('T')[0];
+    form.deadLine.setAttribute('min', today);
 
 
     form.image.addEventListener('change', () => {
+      
       const file = form.image.files[0];
+
+      // Comprobaciones para la imagen.
       if (!file) {
         imgPreview.src = '';
+        imgPreview.classList.add('hidden');
         return;
       }
 
@@ -32,39 +39,76 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 
     form.addEventListener('submit', function (e) {
-        e.preventDefault();
-        const newTask = {
-          title: form.title.value,
-          description: form.description.value,
-          date: form.deadLine.value,
-          image: imgPreview.src
-        };
+      e.preventDefault();
+      const newTask = {
+        title: form.title.value,
+        description: form.description.value,
+        date: form.deadLine.value,
+        image: imgPreview.src
+      };
 
       addTaskCard(newTask)
+
+      // El formulario vuelve a los valores predefinidos.
       form.reset();
       imgPreview.src = "";
       imgPreview.classList.add('hidden');
     });
 
 
+    // Funcion que añade una tarjeta utilizando un template.
     function addTaskCard(newTask) {
       const firstDiv = template.content.cloneNode(true).firstElementChild;
       const h4 = firstDiv.querySelector('#task-title');
       const p = firstDiv.querySelector('#task-description');
       const date = firstDiv.querySelector('#task-date');
       const img = firstDiv.querySelector('img');
-      const btnBorrar = firstDiv.querySelector('#task-delete');
+      const btnDelete = firstDiv.querySelector('#task-delete');
+      const options = firstDiv.querySelector('#task-options')
+      const infoImg = form.querySelector('#image'); //Se guarda el input del formulario principal
 
       h4.textContent = newTask.title;
       p.textContent = newTask.description;
       date.textContent = "Fecha límite: " + newTask.date;
 
-      img.classList.remove('hidden');
+      if(infoImg.value != ""){ // Comprobacion si el input del formulario esta contemplado.
+        // Si esta contemplado lo mostramos.
+        img.classList.remove('hidden');
+        img.src = newTask.image;
+      }
 
-      btnBorrar.addEventListener('click', () => {
+      btnDelete.addEventListener('click', () => {
+        // Borramos la tarjeta
         firstDiv.remove();
       });
-      
+
+      options.addEventListener('change', (e) => {
+        // Movemos la tarjeta dependiendo de la opcion del select
+        const option = e.target.value;
+        moveCard(firstDiv, option);
+      });
+  
+      // El lugar predefinido al añadir la tarjeta.
       divPrincipal.appendChild(firstDiv);
     }
+
+    function moveCard(card, option) {
+    const pending = document.getElementById('divPrincipal');
+    const progress = document.getElementById('task-progress');
+    const completed = document.getElementById('task-completed');
+
+    switch (option) {
+      case '0':
+        pending.appendChild(card);
+        break;
+
+      case '1':
+        progress.appendChild(card);
+        break;
+
+      case '2':
+        completed.appendChild(card);
+        break;
+    }
+  }
 })
